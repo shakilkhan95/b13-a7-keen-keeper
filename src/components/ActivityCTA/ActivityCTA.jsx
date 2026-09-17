4ce88694f81d8d26459dfa50195994abcfa8829f
shakilkhan95 <th.shakilkhan@gmail.com>
2026-09-17 13:21:01 +0400

    Created activity cta page for call text and video button

--- src/components/ActivityCTA/ActivityCTA.jsx
+++ src/components/ActivityCTA/ActivityCTA.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { MdWifiCalling3 } from "react-icons/md";
 
-const ActivityCTA = () => {
+const ActivityCTA = ({friend}) => {
     return (
-        <div>
-            hello
+        <div className="flex flex-col lg:flex-row gap-3">
+            <button className={`btn py-14 px-20 text-2xl flex flex-col justify-center`}><span><MdWifiCalling3/>Call</span></button>
         </div>
     );
 };
