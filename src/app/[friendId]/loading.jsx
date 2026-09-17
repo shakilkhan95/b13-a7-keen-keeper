6cbff79fba5631c4f82fbc9da55e1d31779acc0b
shakilkhan95 <th.shakilkhan@gmail.com>
2026-09-17 13:25:31 +0400

    created loading page for friend details page

---
+++ src/app/[friendId]/loading.jsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center py-10">
+            <span className="loading loading-bars loading-xl text-green-600"></span>
+        </div>
+    );
+};
+
+export default loading;
