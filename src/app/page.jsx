7d0691580ea188f5923bff370af072072fbe6907
shakilkhan95 <th.shakilkhan@gmail.com>
2026-09-19 13:09:04 +0400

    Fixed a responsive problem in home page for mobile device view

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
            {/* Friends Section */}
            <div className="w-11/12 mx-auto">
                 <h1 className="text-3xl font-bold">Your Friends</h1>
-                <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 my-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 my-10">
                     {Friends.map((friend) => <FriendCards key={friend.id} friend={friend} />)}
                 </div>
            </div>
